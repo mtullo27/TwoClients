b2f32d83bd18c460ff1831fa10442b3e6bb164da
Matthew Tullo <mtullo1@binghamton.edu>
2020-10-23 17:56:47 -0400

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -58,7 +58,9 @@ int main() {
 		else
 			message = "Y was recieved before X";
 		//the server responds
-		strcpy(buffer, message.c_str());
+		for(int i = 0; i<message.length(); i++)
+			ack[i] = message[i];
+		ack[message.length()] = '\0';
 		sendto(sockfd, (const char *)buffer, strlen(buffer), 
 			MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
 		break;
