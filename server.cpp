096bc392811ae788b9a90d6f2befafb9fe95b28c
Matthew Tullo <mtullo1@binghamton.edu>
2020-10-23 17:50:17 -0400

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -44,7 +44,6 @@ int main() {
 	
         while (1) {
 		//Receive the client packet along with the address it is coming from
-		cout << count << endl;
 		n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), 
 			MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
 		buffer[n] = '\0';
